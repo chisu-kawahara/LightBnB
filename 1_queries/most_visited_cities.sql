47ea1a1c694d0e6d60ccaf2a75b062f63389c3d7
chisu-kawahara <chisu.kawahara@gmail.com>
2025-07-07 23:44:43 -0700

    Add select queries in most visted cities

--- 1_queries/most_visited_cities.sql
+++ 1_queries/most_visited_cities.sql
@@ -1,6 +1,5 @@
-SELECT properties.city,
-      COUNT(reservations.id) AS total_reservations
+SELECT properties.city, count(reservations) as total_reservations
 FROM reservations
-JOIN properties ON reservations.property_id = properties.id
+JOIN properties ON property_id = properties.id
 GROUP BY properties.city
 ORDER BY total_reservations DESC;
