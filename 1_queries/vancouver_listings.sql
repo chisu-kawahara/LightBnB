e1867bc231fae0deccacdc48fe36d1f4198714d0
chisu-kawahara <chisu.kawahara@gmail.com>
2025-07-07 23:43:36 -0700

    Add  initial SELECT queries for LightBnB

--- 1_queries/vancouver_listings.sql
+++ 1_queries/vancouver_listings.sql
@@ -1,11 +1,8 @@
-SELECT properties.id,
-      properties.title,
-      properties.cost_per_night,
-      AVG(property_reviews.rating) AS average_rating
+SELECT properties.id, title, cost_per_night, avg(property_reviews.rating) as average_rating
 FROM properties
-JOIN property_reviews ON properties.id = property_reviews.property_id
-WHERE city = "Vancouver"
+LEFT JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE '%ancouv%'
 GROUP BY properties.id
-HAVING AVG(property_reviews.rating) >= 4
+HAVING avg(property_reviews.rating) >= 4
 ORDER BY cost_per_night
 LIMIT 10;
